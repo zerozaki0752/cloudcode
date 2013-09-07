c49c69476eb1dc5dbc8d009f5ef46f6d4482eb6f
overlbjn <402309558@qq.com>
2013-09-07 16:37:02 +0800

    1
    1

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,7 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
+
   response.success("Hello world!");
 });
+
